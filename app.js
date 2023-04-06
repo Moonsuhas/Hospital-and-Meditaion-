8e60a32f3207435239fe1ce046d2d77e0347048f
Suhas Moon <suhasmoon11@gmail.com>
2023-04-06 21:01:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+
+		// // Get the form element
+		// var form = document.querySelector("form");
+
+		// // Add an event listener to the form
+		// form.addEventListener("submit", function(event) {
+		// 	// Prevent the default form submission behavior
+		// 	event.preventDefault();
+
+		// 	// Get the medication and time values from the form
+        //     var medication= document.querySelector("#medication").value;
+		// 	var Paitent_Name = document.querySelector("#Paitent_Name").value;
+		// 	var time = document.querySelector("#time").value;
+
+		// 	// Set a reminder for the medication and time
+		// 	setReminder(medication,Paitent_Name, time);
+		// });
+
+		// // Function to set a reminder for the medication and time
+		// function setReminder(medication, Paitent_Name, time) {
+		// 	alert("Reminder: Take " + medication +" at " + time);
+        //     alert("Reminder: Take " + Paitent_Name +" at " + time);
+		// }
+		
+	
